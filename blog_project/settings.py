8fc82df4cefa8a6908496a2fd7882ed69ae81d49
Julian.exe <77165392+Julienexe@users.noreply.github.com>
2026-06-04 09:36:01 +0300

    Change ALLOWED_HOSTS to allow all hosts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-+*z%ri^!u-gka8jyj6j_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
